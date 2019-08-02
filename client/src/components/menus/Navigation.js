3a9fba042417a708eae66e67fce561920cb38439
Ryan O'Connor <rocsteady888@gmail.com>
2019-08-02 17:48:09 -0400

    initial commit

---
+++ client/src/components/menus/Navigation.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
+
+export default function Navigation() {
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" className={classes.title}>
+                Medic Saver
+              </Typography>
+              <Button color="inherit" component={Link} to="/admin-view">Admin View</Button>
+              <Button color="inherit" component={Link} to="/officer-view">Officer View</Button>
+              <Button color="inherit" component={Link} to="/medic-view">Medic View</Button>
+            </Toolbar>
+          </AppBar>
+        </div>
+    )
+}
