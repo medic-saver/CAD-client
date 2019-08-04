1817bfb944105fe35e76f1852d81105e6f7293c0
Ryan O'Connor <rocsteady888@gmail.com>
2019-08-04 09:47:19 -0400

    intial commit

---
+++ client/src/formData/radarData.js
@@ -0,0 +1,32 @@
+export const radarData = [
+  {
+    "disposition": "Unconscious",
+    "ALS": 16,
+    "BLS": 2
+  },
+  {
+    "disposition": "Chest Pains",
+    "ALS": 27,
+    "BLS": 0
+  },
+  {
+    "disposition": "Breathing Problems",
+    "ALS": 21,
+    "BLS": 4
+  },
+  {
+    "disposition": "Transfer",
+    "ALS": 30,
+    "BLS": 25
+  },
+  {
+    "disposition": "Sick Person",
+    "ALS": 15,
+    "BLS": 16
+  },
+  {
+    "disposition": "MVC with Injuries",
+    "ALS": 2,
+    "BLS": 16
+  }
+]
