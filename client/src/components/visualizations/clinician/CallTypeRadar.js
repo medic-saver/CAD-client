0910ad722b3dc5e180315b34291d01a760658349
Ryan O'Connor <rocsteady888@gmail.com>
2019-08-04 09:47:02 -0400

    intial commit

---
+++ client/src/components/visualizations/clinician/CallTypeRadar.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { ResponsiveRadar } from "@nivo/radar";
+import { radarData } from '../../../formData/radarData';
+// make sure parent container have a defined height when using
+// responsive component, otherwise height will be 0 and
+// no chart will be rendered.
+// website examples showcase many properties,
+// you'll often use just a few of them.
+export default function CallTypeRadar() {
+  return (
+    <div style={{ height: "350px" }}>
+      <ResponsiveRadar
+        data={radarData}
+        keys={["ALS", "BLS"]}
+        indexBy="disposition"
+        maxValue="auto"
+        margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
+        curve="linearClosed"
+        borderWidth={2}
+        borderColor={{ from: "color" }}
+        gridLevels={5}
+        gridShape="circular"
+        gridLabelOffset={36}
+        enableDots={true}
+        dotSize={10}
+        dotColor={{ theme: "background" }}
+        dotBorderWidth={2}
+        dotBorderColor={{ from: "color" }}
+        enableDotLabel={true}
+        dotLabel="value"
+        dotLabelYOffset={-12}
+        colors={{ scheme: "nivo" }}
+        fillOpacity={0.25}
+        blendMode="multiply"
+        animate={true}
+        motionStiffness={90}
+        motionDamping={15}
+        isInteractive={true}
+        legends={[
+          {
+            anchor: "top-left",
+            direction: "column",
+            translateX: -50,
+            translateY: -40,
+            itemWidth: 80,
+            itemHeight: 20,
+            itemTextColor: "#999",
+            symbolSize: 12,
+            symbolShape: "circle",
+            effects: [
+              {
+                on: "hover",
+                style: {
+                  itemTextColor: "#000"
+                }
+              }
+            ]
+          }
+        ]}
+      />
+    </div>
+  );
+}
