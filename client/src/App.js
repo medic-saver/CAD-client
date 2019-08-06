c64e3e0c165f5460da6b6f5364e0499050a7b380
Ryan O'Connor <rocsteady888@gmail.com>
2019-08-06 12:31:37 -0400

    new route for system view

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Landing from "./views/Landing";
 import AdminView from "./views/AdminView";
 import OfficerView from "./views/OfficerView";
 import ClinicianView from "./views/ClinicianView";
+import SystemView from "./views/SystemView";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Route exact path="/admin-view" component={AdminView} />
       <Route exact path="/officer-view" component={OfficerView} />
       <Route exact path="/clinician-view" component={ClinicianView} />
+      <Route exact path="/system-view" component={SystemView} />
     </Router>
   );
 }
