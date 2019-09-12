e3027de6d35e651d9eea03f5224b24f0a9f29ded
Rachel Stemerman <rstemerman@gmail.com>
2019-09-12 19:55:03 -0400

    update db code

--- server/backend/api/views.py
+++ server/backend/api/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request
 from .models import Data
 from io import TextIOWrapper
 import csv
+import pandas as pd
 
 main = Blueprint('main', __name__)
 
@@ -9,10 +10,12 @@ main = Blueprint('main', __name__)
 def upload_data():
     if request.method == 'POST':
         csvfile = request.files['file']
-        csvfile = TextIOWrapper(csvfile, encoding='utf-8')
-        csvreader = csv.reader(csvfile, delimiter=',')
-        for row in csvreader:
-            data = Data(id=row[0], last_name=row[1])
+        data = pd.read_csv(csvfile)
+        data.to_sql(con=engine, index_label='id', name=cdb1.__data__, if_exists='append')
+        # csvfile = TextIOWrapper(csvfile, encoding='utf-8')
+        # csvreader = csv.reader(csvfile, delimiter=',')
+        # for row in csvreader:
+        #     data = Data(id=row[0], last_name=row[1])
 
             db.session.add(data)
             db.session.commit()
